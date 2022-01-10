2947aabe0090ca8b0485787ef6bd42836a907363
DESKTOP-C0H936H\iiooi <iiooii1994@naver.com>
2022-01-10 23:56:38 +0900

    로그인 기능과 회원가입 페이지 모두 login에 있습니당~!

--- static/myjs.js
+++ static/myjs.js
@@ -4,31 +4,31 @@ function logout() {
     window.location.href = '/login'
 }
 
-function sign_in() {
-    let username = $("#input-username").val()
-    let password = $("#input-password").val()
+function login() {
+    let id = $("#input-id").val()
+    let pw = $("#input-pw").val()
 
-    if (username == "") {
+    if (id == "") {
         $("#help-id-login").text("아이디를 입력해주세요.")
-        $("#input-username").focus()
+        $("#input-id").focus()
         return;
     } else {
         $("#help-id-login").text("")
     }
 
-    if (password == "") {
+    if (pw == "") {
         $("#help-password-login").text("비밀번호를 입력해주세요.")
-        $("#input-password").focus()
+        $("#input-pw").focus()
         return;
     } else {
         $("#help-password-login").text("")
     }
     $.ajax({
         type: "POST",
-        url: "/sign_in",
+        url: "/api/login",
         data: {
-            username_give: username,
-            password_give: password
+            id_give: id,
+            pw_give: pw
         },
         success: function (response) {
             if (response['result'] == 'success') {
@@ -44,50 +44,51 @@ function sign_in() {
 }
 
 
-function toggle_sign_up() {
-    $("#sign-up-box").toggleClass("is-hidden")
+function toggle_regist() {
+    $("#regist-box").toggleClass("is-hidden")
     $("#div-sign-in-or-up").toggleClass("is-hidden")
     $("#btn-check-dup").toggleClass("is-hidden")
     $("#help-id").toggleClass("is-hidden")
-    $("#help-password").toggleClass("is-hidden")
-    $("#help-password2").toggleClass("is-hidden")
+    $("#help-pw").toggleClass("is-hidden")
+    $("#help-pw2").toggleClass("is-hidden")
 }
 
-function is_nickname(asValue) {
+function is_id(asValue) {
     var regExp = /^(?=.*[a-zA-Z])[-a-zA-Z0-9_.]{2,10}$/;
     return regExp.test(asValue);
 }
 
-function is_password(asValue) {
+function is_pw(asValue) {
     var regExp = /^(?=.*\d)(?=.*[a-zA-Z])[0-9a-zA-Z!@#$%^&*]{8,20}$/;
     return regExp.test(asValue);
 }
 
 function check_dup() {
-    let username = $("#input-username").val()
-    console.log(username)
-    if (username == "") {
+    let id = $("#input-id").val()
+    console.log(id)
+    if (id == "") {
         $("#help-id").text("아이디를 입력해주세요.").removeClass("is-safe").addClass("is-danger")
-        $("#input-username").focus()
+        $("#input-id").focus()
+
         return;
     }
-    if (!is_nickname(username)) {
+    if (!is_id(id)) {
         $("#help-id").text("아이디의 형식을 확인해주세요. 영문과 숫자, 일부 특수문자(._-) 사용 가능. 2-10자 길이").removeClass("is-safe").addClass("is-danger")
-        $("#input-username").focus()
+        $("#input-id").focus()
         return;
     }
     $("#help-id").addClass("is-loading")
     $.ajax({
         type: "POST",
-        url: "/sign_up/check_dup",
+        url: "/register/check_dup",
         data: {
-            username_give: username
+            id_give: id
         },
         success: function (response) {
 
             if (response["exists"]) {
                 $("#help-id").text("이미 존재하는 아이디입니다.").removeClass("is-safe").addClass("is-danger")
-                $("#input-username").focus()
+                $("#input-id").focus()
             } else {
                 $("#help-id").text("사용할 수 있는 아이디입니다.").removeClass("is-danger").addClass("is-success")
             }
@@ -97,11 +98,11 @@ function check_dup() {
     });
 }
 
-function sign_up() {
-    let username = $("#input-username").val()
-    let password = $("#input-password").val()
-    let password2 = $("#input-password2").val()
-    console.log(username, password, password2)
+function regist() {
+    let id = $("#input-id").val()
+    let pw = $("#input-pw").val()
+    let pw2 = $("#input-pw2").val()
+    console.log(id, pw, pw2)
 
 
     if ($("#help-id").hasClass("is-danger")) {
@@ -112,34 +113,34 @@ function sign_up() {
         return;
     }
 
-    if (password == "") {
-        $("#help-password").text("비밀번호를 입력해주세요.").removeClass("is-safe").addClass("is-danger")
-        $("#input-password").focus()
+    if (pw == "") {
+        $("#help-pw").text("비밀번호를 입력해주세요.").removeClass("is-safe").addClass("is-danger")
+        $("#input-pw").focus()
         return;
-    } else if (!is_password(password)) {
-        $("#help-password").text("비밀번호의 형식을 확인해주세요. 영문과 숫자 필수 포함, 특수문자(!@#$%^&*) 사용가능 8-20자").removeClass("is-safe").addClass("is-danger")
-        $("#input-password").focus()
+    } else if (!is_pw(pw)) {
+        $("#help-pw").text("비밀번호의 형식을 확인해주세요. 영문과 숫자 필수 포함, 특수문자(!@#$%^&*) 사용가능 8-20자").removeClass("is-safe").addClass("is-danger")
+        $("#input-pw").focus()
         return
     } else {
-        $("#help-password").text("사용할 수 있는 비밀번호입니다.").removeClass("is-danger").addClass("is-success")
+        $("#help-pw").text("사용할 수 있는 비밀번호입니다.").removeClass("is-danger").addClass("is-success")
     }
-    if (password2 == "") {
-        $("#help-password2").text("비밀번호를 입력해주세요.").removeClass("is-safe").addClass("is-danger")
-        $("#input-password2").focus()
+    if (pw2 == "") {
+        $("#help-pw2").text("비밀번호를 입력해주세요.").removeClass("is-safe").addClass("is-danger")
+        $("#input-pw2").focus()
         return;
-    } else if (password2 != password) {
-        $("#help-password2").text("비밀번호가 일치하지 않습니다.").removeClass("is-safe").addClass("is-danger")
-        $("#input-password2").focus()
+    } else if (pw2 != pw) {
+        $("#help-pw2").text("비밀번호가 일치하지 않습니다.").removeClass("is-safe").addClass("is-danger")
+        $("#input-pw2").focus()
         return;
     } else {
-        $("#help-password2").text("비밀번호가 일치합니다.").removeClass("is-danger").addClass("is-success")
+        $("#help-pw2").text("비밀번호가 일치합니다.").removeClass("is-danger").addClass("is-success")
     }
     $.ajax({
         type: "POST",
-        url: "/sign_up/save",
+        url: "/api/register",
         data: {
-            username_give: username,
-            password_give: password
+            username_give: id,
+            password_give: pw
         },
         success: function (response) {
             alert("가입 완료~ 도쿄 보러가요~ ")
